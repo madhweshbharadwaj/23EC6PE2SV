e5bdbaddc34f8d13070302be1d319dbacb97434a
madhweshbharadwaj <69860797+madhweshbharadwaj@users.noreply.github.com>
2026-02-14 00:34:24 +0530

    Update digital clock_tb.sv

--- AAT1/EDA/Task2/digital clock_tb.sv
+++ AAT1/EDA/Task2/digital clock_tb.sv
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 //File       : digital_clock_tb.sv
-//Author     : Nirvara Reddy/1BM23EC170
+//Author     : Madhwesh Bharadwaj CV /1BM23EC135
 //Created    : 2026-02-10
 //Module     : tb
 //Project    : SystemVerilog and Verification (23EC6PE2SV),
